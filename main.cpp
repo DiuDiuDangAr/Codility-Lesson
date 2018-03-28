193e61c8646f4d15d7dccd0bec3c3d0fd125bcce
DiuDiuDangAr <r05921083@ntu.edu.tw>
2018-03-29 00:06:25 +0800

    add main cpp to call the other functions

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char command[];
+	cout << "List of command:" << endl;
+	cout << "\t B - Basic Data Structure" << endl;
+	cout << "\t R - Recursion" << endl;
+	cout << "\t S1 - Searching" << endl;
+	cout << "\t S2 - Sorting" << endl;
+	cout << "\t G - Graph" << endl;
+
+	cin >> command;
+
+}
+
+
